106df30ebe8cd9dae92d83bb6258aaf8f4b2e223
mildlybrutal <singuakash23@lpu.in>
2024-08-25 01:39:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Music App
+
+- Discover Page: Has SongCards that display the song's name, artist, and album art. and A dropdown menu that allows the user to filter the songs by genre.
---
+++ src/pages/Discover.jsx
@@ -0,0 +1,13 @@
+import { Error, Loading, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+	console.log(genres);
+	return (
+		<div>
+			<h1>Discover</h1>
+		</div>
+	);
+};
+
+export default Discover;
